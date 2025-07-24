40fdca65cd49554d5623b739079ec2b4d020fd31
PTHARRISH <harrishpt@gmail.com>
2025-07-24 23:30:34 +0530

    # 2. path parameter

--- main.py
+++ main.py
@@ -12,3 +12,31 @@ def view():
 @app.get("/path/{id}")
 def path_parameter(id):
     return f"The path id you passed is: {id}"
+
+
+emp = [
+    {
+        "id": 1,
+        "name": "Admin",
+        "location": "Bengaluru",
+    },
+    {
+        "id": 16,
+        "name": "Harrish PT",
+        "location": "Bengaluru",
+    },
+]
+
+
+@app.get("/employee/{id}")
+def employee_details(id: int):
+    for e in emp:
+        if e["id"] == id:
+            return e
+
+
+@app.get("/emp_name/{name}")
+def employee_name(name: str):
+    for e in emp:
+        if e["name"] == name:
+            return e
